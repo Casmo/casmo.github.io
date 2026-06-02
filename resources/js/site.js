1a3937002faa40437b35b86b9408f1b2ca7629c6
Mathieu <mathieu@ux.nl>
2026-06-02 16:15:21 +0200

    Add yaml support

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,7 +1,9 @@
 // This is all you.
 import hljs from 'highlight.js/lib/core';
 import php from 'highlight.js/lib/languages/php';
+import yaml from 'highlight.js/lib/languages/yaml';
 
 hljs.registerLanguage('php', php);
+hljs.registerLanguage('yaml', yaml);
 
 hljs.highlightAll();
