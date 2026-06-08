906fe3127f0756b69043fd42c49f29c2450233db
Mathieu <mathieu@ux.nl>
2026-06-08 16:33:15 +0200

    add html

--- resources/js/site.js
+++ resources/js/site.js
@@ -2,8 +2,10 @@
 import hljs from 'highlight.js/lib/core';
 import php from 'highlight.js/lib/languages/php';
 import yaml from 'highlight.js/lib/languages/yaml';
+import xml from 'highlight.js/lib/languages/xml';
 
 hljs.registerLanguage('php', php);
 hljs.registerLanguage('yaml', yaml);
+hljs.registerLanguage('xml', xml);
 
 hljs.highlightAll();
