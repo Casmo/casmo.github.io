d820966744db0dd0ecef79d2635f195f915cffa7
Mathieu <mathieu@fellicht.nl>
2026-07-29 22:19:24 +0200

    Change class name for knowledge div in default view

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -24,7 +24,7 @@ text-zinc-400 px-4 sm:px-6 lg:px-8 max-w-2xl mx-auto">
 
         @yield('body')
 
-        <div class="flex items-center text-sm text-white">
+        <div class="flex items-center knowledge text-white">
             <img src="https://mathieuderuiter.nl/assets/knowledge/quake-1-pixel-logo.png" class="mr-2" height="16" width="16" style="image-rendering: crisp-edges;" /> Quake 1 was released on July 22, 1994.
         </div>
     </body>
