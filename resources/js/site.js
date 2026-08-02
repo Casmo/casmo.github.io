f135a544858fb066fec541e72c6be7a56c0f71bd
Mathieu de Ruiter <mathieu@fellicht.nl>
2026-08-02 12:22:39 +0200

    Syntax & highlight json

--- resources/js/site.js
+++ resources/js/site.js
@@ -3,9 +3,11 @@ import hljs from 'highlight.js/lib/core';
 import php from 'highlight.js/lib/languages/php';
 import yaml from 'highlight.js/lib/languages/yaml';
 import xml from 'highlight.js/lib/languages/xml';
+import json from 'highlight.js/lib/languages/json';
 
 hljs.registerLanguage('php', php);
 hljs.registerLanguage('yaml', yaml);
 hljs.registerLanguage('xml', xml);
+hljs.registerLanguage('json', json);
 
 hljs.highlightAll();
